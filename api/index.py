4b38aa061bb644236085ab6b9b11c0be159fec3d
Mishal <mishal0404@gmail.com>
2024-07-27 15:01:43 +0530

    reset api

--- api/index.py
+++ api/index.py
@@ -1,21 +1,15 @@
-import json
-from fastapi import FastAPI, Request, HTTPException, Depends
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-import firebase_admin
-from firebase_admin import auth, credentials, initialize_app
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi.security import HTTPBearer
 
 
-from os import getenv, path
+from os import path
 from dotenv import load_dotenv
 
 app_base = path.dirname(__file__)
 app_root = path.join(app_base, '../')
 
 load_dotenv(dotenv_path=path.join(app_root, '.env'))
-
-cred = credentials.Certificate(json.loads(getenv("SERVICE_ACCOUNT_KEY")))
-firebase_admin.initialize_app(cred)
 
 app = FastAPI()
 
@@ -31,14 +25,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
-    try:
-        decoded_token = auth.verify_id_token(credentials.credentials)
-        return decoded_token
-    except Exception as e:
-        raise HTTPException(status_code=401, detail="Invalid token")
-
 @app.get("/api/python")
-def hello_world(request: Request, current_user: dict = Depends(get_current_user)):
-    mystr = f"Hello, {current_user['name']}. This is a random sentence"
+def hello_world(request: Request):
+    mystr = f"Hello, {request['name']}. This is a random sentence"
     return {"message":mystr}
